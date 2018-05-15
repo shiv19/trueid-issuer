8526db960a9ce564475ddc567d5d0ae6ec8893d2
Shiva Prasad <mailshiva19@gmail.com>
2018-05-15 16:35:30 +0530

    edit user

--- src/app/user/view-user/view-user.component.ts
+++ src/app/user/view-user/view-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import trueID from '../../web3/trueID';
 
 @Component({
   selector: 'app-view-user',
@@ -9,7 +10,9 @@ import { UserService } from '../../services/user.service';
 })
 export class ViewUserComponent implements OnInit {
 
-  user = {};
+  user: any = {};
+  addresses: any;
+
   constructor(private route: ActivatedRoute, private userService: UserService, private router: Router) { }
 
   ngOnInit() {
@@ -17,10 +20,30 @@ export class ViewUserComponent implements OnInit {
     if (!this.user) {
       this.router.navigate(['/users']);
     }
+
+    this.addresses = localStorage.getItem('accounts');
+
+    if (!this.addresses) {
+        this.router.navigate(['/users']);
+    } else {
+        this.addresses = JSON.parse(this.addresses);
+    }
   }
 
-  onSave(value) {
-    console.log(value);
+  async onSave(value) {
+    await trueID.methods.editUser(
+        this.user.address,
+        value.fullName,
+        value.fatherName,
+        value.motherName,
+        value.contactAddress,
+        value.gender,
+        value.birthdate,
+        value.country
+    ).send({
+        gas: 2000000,
+        from: this.addresses[0]
+    });
   }
 
 }
